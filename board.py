0fa836828c9e3f0991f03b4d96318a26c916b829
m-salari <mslri79kh@gmail.com>
2023-06-16 23:53:54 +0330

    add method remove and score

--- board.py
+++ board.py
@@ -53,6 +53,7 @@ class board(QMainWindow):
         self.start_bead_move = None
         self.end_bead_move = None
         self.color_change_bead = None
+        self.flag_remove = False
 
         self.init_board()
 
@@ -90,76 +91,89 @@ class board(QMainWindow):
         background-position: center;""")
 
     def draw_pieces(self):
-        # piece(QPushButton(self), 30, 36, 'a', 0, [(2,2)], 'white')
-        self.main_board[0][0]: piece = piece(QPushButton(self), 30, 36, row=0, col=0, lst_neighbors=[(0, 3), (3, 0), (1, 1)])
-        self.main_board[0][3]: piece = piece(QPushButton(self), 378, 36, row=0, col=3, lst_neighbors=[(0, 0), (0, 6), (1, 3)])
-        self.main_board[0][6]: piece = piece(QPushButton(self), 726, 36, row=0, col=6, lst_neighbors=[(0, 3), (4, 6), (1, 5)])
+        self.main_board[0][0]: piece = piece(QPushButton(self), 30, 36, row=0, col=0,
+                                             lst_neighbors=[(0, 3), (3, 0), (1, 1)])
+        self.main_board[0][3]: piece = piece(QPushButton(self), 378, 36, row=0, col=3,
+                                             lst_neighbors=[(0, 0), (0, 6), (1, 3)])
+        self.main_board[0][6]: piece = piece(QPushButton(self), 726, 36, row=0, col=6,
+                                             lst_neighbors=[(0, 3), (4, 6), (1, 5)])
 
-        self.main_board[1][1]: piece = piece(QPushButton(self), 112, 98, row=1, col=1, lst_neighbors=[(0, 0), (2, 2), (1, 3), (3, 1)])
-        self.main_board[1][3]: piece = piece(QPushButton(self), 378, 94, row=1, col=3,  lst_neighbors=[(1, 1), (1, 5), (0, 3), (2, 3)])
-        self.main_board[1][5]: piece = piece(QPushButton(self), 642, 96, row=1, col=5,  lst_neighbors=[(1, 3), (4, 5), (0, 6), (2, 4)])
-
-        # self.main_board[2][0]: piece = piece(QPushButton(self), 'x', 'y', 'c', 0, [(2, 1), (3, 1,)])
-        # self.main_board[2][0]: piece = piece(QPushButton(self), 206, 162, 'c', 2)
-        #################################################################################
+        self.main_board[1][1]: piece = piece(QPushButton(self), 112, 98, row=1, col=1,
+                                             lst_neighbors=[(0, 0), (2, 2), (1, 3), (3, 1)])
+        self.main_board[1][3]: piece = piece(QPushButton(self), 378, 94, row=1, col=3,
+                                             lst_neighbors=[(1, 1), (1, 5), (0, 3), (2, 3)])
+        self.main_board[1][5]: piece = piece(QPushButton(self), 642, 96, row=1, col=5,
+                                             lst_neighbors=[(1, 3), (4, 5), (0, 6), (2, 4)])
 
     def moving(self):
-        self.main_board[0][0].button.clicked.connect(lambda: self.allow_to_move(0, 0))
-        self.main_board[0][3].button.clicked.connect(lambda: self.allow_to_move(0, 3))
-        self.main_board[0][6].button.clicked.connect(lambda: self.allow_to_move(0, 6))
+        # a
+        self.main_board[0][0].button.clicked.connect(
+            lambda: self.allow_to_move(0, 0) if not self.flag_remove else self.remove_bead(0, 0))
 
-        self.main_board[1][1].button.clicked.connect(lambda: self.allow_to_move(1, 1))
-        self.main_board[1][3].button.clicked.connect(lambda: self.allow_to_move(1, 3))
-        self.main_board[1][5].button.clicked.connect(lambda: self.allow_to_move(1, 5))
+        self.main_board[0][3].button.clicked.connect(
+            lambda: self.allow_to_move(0, 3) if not self.flag_remove else self.remove_bead(0, 3))
 
+        self.main_board[0][6].button.clicked.connect(
+            lambda: self.allow_to_move(0, 6) if not self.flag_remove else self.remove_bead(0, 6))
 
-        # self.main_board[2][0].button.clicked.connect(lambda: self.allow_to_move(2, 0))
+        # b
+        self.main_board[1][1].button.clicked.connect(
+            lambda: self.allow_to_move(1, 1) if not self.flag_remove else self.remove_bead(1, 1))
+
+        self.main_board[1][3].button.clicked.connect(
+            lambda: self.allow_to_move(1, 3) if not self.flag_remove else self.remove_bead(1, 3))
+
+        self.main_board[1][5].button.clicked.connect(
+            lambda: self.allow_to_move(1, 5) if not self.flag_remove else self.remove_bead(1, 5))
 
     # def mouseMoveEvent(self, event):
     #     print(f"x mouse: {event.x()} and y mouse: {event.y()}")
 
+    def remove_bead(self, row, col):
+        button: piece = self.main_board[row][col]
+        if self.player.color != button.color:
+            button.color = 'white'
+            button.change_button_color()
+            self.flag_remove = False
+        else:
+            print('pass')
+
     def check_score(self):
-        # todo: باید مطابق با main board درست شود
         try:
-            if self.button_row_a[0].color == self.button_row_a[1].color == self.button_row_a[
-                2].color != 'white':  # row a
+            if self.main_board[0][0].color == self.main_board[0][3].color == self.main_board[0][6].color != 'white':  # row a
                 print('score a')
+                self.flag_remove = True
 
-            if self.button_row_b[0].color == self.button_row_b[1].color == self.button_row_b[
-                2].color != 'white':  # row b
+            if self.main_board[1][1].color == self.main_board[1][3].color == self.main_board[1][5].color != 'white':  # row b
                 print('score b')
+                self.flag_remove = True
 
-            if self.button_row_c[0].color == self.button_row_c[1].color == self.button_row_c[
-                2].color != 'white':  # row c
+            if self.main_board[2][2].color == self.main_board[2][3].color == self.main_board[2][4].color != 'white':  # row c
                 print('score c')
+                self.flag_remove = True
 
-            if self.button_row_d[0].color == self.button_row_d[1].color == self.button_row_d[
-                2].color != 'white':  # row d1
+            if self.main_board[3][0].color == self.main_board[3][1].color == self.main_board[3][2].color != 'white':  # row d1
                 print('score d1')
+                self.flag_remove = True
 
-            if self.button_row_d[3].color == self.button_row_d[4].color == self.button_row_d[
-                5].color != 'white':  # row d2
+            if self.main_board[3][4].color == self.main_board[3][5].color == self.main_board[3][6].color != 'white':  # row d2
                 print('score d2')
+                self.flag_remove = True
 
-            if self.button_row_e[0].color == self.button_row_e[1].color == self.button_row_e[
-                2].color != 'white':  # row e
+            if self.main_board[4][2].color == self.main_board[4][3].color == self.main_board[4][4].color != 'white':  # row e
                 print('score e')
+                self.flag_remove = True
 
-            if self.button_row_f[0].color == self.button_row_f[1].color == self.button_row_f[
-                2].color != 'white':  # row f
+            if self.main_board[5][1].color == self.main_board[5][3].color == self.main_board[5][5].color != 'white':  # row f
                 print('score f')
+                self.flag_remove = True
 
-            if self.button_row_g[0].color == self.button_row_g[1].color == self.button_row_g[
-                2].color != 'white':  # row g
+            if self.main_board[6][0].color == self.main_board[6][3].color == self.main_board[6][6].color != 'white':  # row g
                 print('score g')
+                self.flag_remove = True
+
 
             # **  full row **
-
-            # if self.button_row_a[0].color == self.button_row_d[0].color == self.button_row_g[0].color != 'white':  # col 0
-            #     print('score col 0')
-
-            # if self.button_row_b[0].color == self.button_row_d[1].color == self.button_row_f[0].color != 'white':  # col 1
-            #     print('score col 1')
 
         except:
             pass
@@ -173,7 +187,7 @@ class board(QMainWindow):
                 if self.main_board[row][col].color == 'white':
                     lst_neighbors.append((row, col))
 
-            except: # به خاطر اینکه هنوز صفحه کامل نیستو بعد از کامل شدن try برداشته شود
+            except:  # به خاطر اینکه هنوز صفحه کامل نیستو بعد از کامل شدن try برداشته شود
                 pass
 
         return lst_neighbors
@@ -189,8 +203,8 @@ class board(QMainWindow):
                         self.player1.number_bead -= 1
                         self.player = self.player1
 
-                    # todo: add if number_head is last --> switch
-                    if self.player2.number_bead > 0:  # switch turn
+                                                    #  if last bead --> switch
+                    if self.player2.number_bead > 0 or self.player1.number_bead == 0:  # switch turn
                         self.player1.turn = 0
                         self.player2.turn = 1
 
@@ -198,14 +212,15 @@ class board(QMainWindow):
                     if self.player2.number_bead > 0:
                         self.player2.number_bead -= 1
                         self.player = self.player2
-
-                    if self.player1.number_bead > 0:  # switch turn
+                                                    # if last bead --> switch
+                    if self.player1.number_bead > 0 or self.player2.number_bead == 0:  # switch turn
                         self.player2.turn = 0
                         self.player1.turn = 1
 
                 button.color = self.player.color
                 button.change_button_color()
-                # self.check_score()
+
+                self.check_score()
                 # print(f'{self.button_row_b[0].color}, {self.button_row_b[1].color}, {self.button_row_b[2].color}')
 
         elif self.player1.number_bead == 0 and self.player2.number_bead == 0:
@@ -247,6 +262,7 @@ class board(QMainWindow):
                     self.color_change_bead = None
                     self.lst_neighbors = []
 
+                    self.check_score()
 
 App = QApplication(sys.argv)  # create pyqt5 app
 window = board()  # create the instance of our Window
